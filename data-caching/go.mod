5edb9e864eecea8da86cab878ec14a7816c617ea
pillaiharish <harishpillai1994@gmail.com>
2024-04-26 23:38:12 +0530

    Added data caching example

---
+++ data-caching/go.mod
@@ -0,0 +1,3 @@
+module data-caching
+
+go 1.22.2
---
+++ data-caching/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var cache = make(map[string]interface{})
+
+// Query for data if not found
+func expensiveOperation(key string) interface{} {
+	//Run query to fetch data
+	time.Sleep(2 * time.Second)
+	fmt.Println("Executing Query...")
+	return "Result for " + key
+}
+
+// Get the the value for given key
+func getValue(key string) interface{} {
+	value, found := cache[key]
+	if !found {
+		value = expensiveOperation(key)
+		cache[key] = value
+	}
+	return value
+}
+
+func main() {
+	fmt.Println("Fetching Data...")
+	// Since first value is not present getValue() will call expensiveOperation()
+	fmt.Println(getValue("myData"))
+	fmt.Println("Fetching Data again...")
+	// now value present in cache, expensiveOperation() will not be called
+	fmt.Println(getValue("myData")) // immediate output
+
+}
